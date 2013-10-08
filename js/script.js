9a2d22ea0a73de49683d0a1289d5afc1b2349f02
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2013-10-09 01:56:41 +0530

    make changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $('a.menu').click(function () {
 	// body...
 	$('.section').hide();
+	$('.section').removeClass('animated');
 	$('#'+$(this).attr('menu-id')).show();
+	$('#'+$(this).attr('menu-id')).addClass('animated pulse');
 })
