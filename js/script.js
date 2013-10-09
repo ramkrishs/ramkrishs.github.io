dfbaf68f629a50b01514240564079e17cef7f72f
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2013-10-09 23:42:13 +0530

    change val

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $('a.menu').click(function () {
 	// body...
 	$('.section').hide();
-	$('.section').removeClass('animated');
 	$('#'+$(this).attr('menu-id')).show();
-	$('#'+$(this).attr('menu-id')).addClass('animated pulse');
 })
+$('#home').addClass('animated bounceInDown');
