886cc9e900e2e67f1650376bdaffa55897504fe7
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2013-10-28 21:40:46 +0530

    resize fix

--- js/script.js
+++ js/script.js
@@ -3,9 +3,31 @@ $('a.menu').click(function () {
 	$('.section').hide();
 	$('#'+$(this).attr('menu-id')).show();
 })
-$('#home').removeClass('animated').css("visibility", "visible").addClass('animated bounceIn');
-$('#about .info').removeClass('animated').css("visibility", "visible").addClass('animated bounceInDown');
-$('#world .info').removeClass('animated').css("visibility", "visible").addClass('animated bounceInRight');
-$('#contact .email').removeClass('animated').css("visibility", "visible").addClass('animated bounceIn');
-$('#contact .phone').removeClass('animated').css("visibility", "visible").addClass('animated bounceIn');
 
+var minWidth =480;
+
+function contentAnimate () {
+		if($(window).width() > minWidth)
+		{
+		$('#home').removeClass('animated').css("visibility", "visible").addClass('animated bounceIn');
+		$('#about .info').removeClass('animated').css("visibility", "visible").addClass('animated bounceInDown');
+		$('#world .info').removeClass('animated').css("visibility", "visible").addClass('animated bounceInRight');
+		$('#contact .email').removeClass('animated').css("visibility", "visible").addClass('animated bounceIn');
+		$('#contact .phone').removeClass('animated').css("visibility", "visible").addClass('animated bounceIn');
+		}
+		else
+		{
+			$('#home').removeClass('animated').css("visibility","visible");
+			$('#about .info').removeClass('animated');
+			$('#world .info').removeClass('animated');
+			$('#contact .email').removeClass('animated');
+			$('#contact .phone').removeClass('animated');
+		}
+
+}
+$(document).ready(function() {
+		contentAnimate();
+		$(window).resize(function() {
+		contentAnimate();
+		});
+});
