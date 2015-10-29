6472d82319bdc6762166821bb9d1652977bfd226
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2015-10-29 02:18:01 -0500

    change in sidebar

--- js/script.js
+++ js/script.js
@@ -6,9 +6,20 @@ window.onload = function() {
     if (sidebar.className.indexOf("slideIn") != -1) {
       sidebar.className = sidebar.className.replace(" slideIn", "");
     } else {
-     sidebar.className = sidebar.className + " slideIn"; 
+     sidebar.className = sidebar.className + " slideIn";
     }
   }
 
   sidebarButton.addEventListener("click", openSidebar, false);
 };
+
+$(document).ready(function(){
+  $(".nav-title").hover(function () {
+
+      var colorArray = new Array("#2980b9","#e67e22","#8e44ad","#16a085","#2ecc71","#c0392b");
+      var randomNumber = Math.floor((Math.random() * 6) + 0);
+      console.log(colorArray[2]);
+      $(this).css("color",colorArray[randomNumber]);
+    }
+  );
+});
